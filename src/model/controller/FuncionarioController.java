263b30e173bf6f5990ad5e87ac4aa66e866673ff
Thiago <augusto.dev01@gmail.com>
2025-12-05 17:21:16 -0300

    mais algumas coisinhas

--- src/model/controller/FuncionarioController.java
+++ src/model/controller/FuncionarioController.java
@@ -2,7 +2,7 @@ package model.controller;
 
 import java.util.Scanner;
 import model.service.FuncionarioService;
-import model.dao.DaoFactory;
+
 
 public class FuncionarioController {
 
@@ -11,10 +11,7 @@ public class FuncionarioController {
     private final Scanner sc = new Scanner(System.in);
 
     public FuncionarioController(FuncionarioService funcionarioService) {
-        this.funcionarioService = new FuncionarioService(
-            DaoFactory.createUsuarioDAO(),
-            DaoFactory.createFuncionarioDAO()
-        );
+        this.funcionarioService = funcionarioService;
     }
 
     public void cadastrarNovoFuncionario(){
@@ -25,7 +22,7 @@ public class FuncionarioController {
         String cargo = sc.nextLine();
         System.out.println("Digite o email: ");
         String email = sc.nextLine();
-        System.out.println("Digite a senha: ");
+        System.out.println("Digite uma senha: ");
         String senha = sc.nextLine();
 
         funcionarioService.cadastrarFuncionario(nome, senha, email, cargo);
@@ -33,9 +30,44 @@ public class FuncionarioController {
     }
 
     public void atualizarFuncionario(){
-
         
+        System.out.println("Digite o ID do Funcionário que deseja atualizar: ");
+        int id = Integer.parseInt(sc.nextLine());
+        var funcionario = funcionarioService.buscarFuncionarioPorId(id);
+        if(funcionario == null){
+            System.out.println("Funcionário não encontrado!");
+            return;
+        }
+        System.out.println("Digite o novo cargo: ");
+        String cargo =  sc.nextLine();
+        funcionario.setCargo(cargo);
+        funcionarioService.atualizarFuncionario(funcionario);
+        System.out.println("Funcionário atualizado com sucesso!");
+    }
 
+    public void listarFuncionarios(){
+        var funcionarios = funcionarioService.listarFuncionarios();
+        for(var func : funcionarios){
+            System.out.println(func);
+        }
+    }
+
+    public void deletarFuncionario(){
+        System.out.println("Digite o ID do Funcionário que deseja deletar: ");
+        int id = Integer.parseInt(sc.nextLine());
+        funcionarioService.deletarFuncionario(id);
+        System.out.println("Funcionário deletado com sucesso!");
+    }
+
+    public void buscarFuncionarioPorId(){
+        System.out.println("Digite o ID do Funcionário que deseja buscar: ");
+        int id = Integer.parseInt(sc.nextLine());
+        var funcionario = funcionarioService.buscarFuncionarioPorId(id);
+        if(funcionario == null){
+            System.out.println("Funcionário não encontrado!");
+            return;
+        }
+        System.out.println(funcionario);
     }
 
 
