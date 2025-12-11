cce350a167abd2aa628b7761f9c1616770661e7f
rrodriguess20 <rodriguess.rebeca20@gmail.com>
2025-12-11 19:48:06 -0300

    consertando erro em exception

--- src/model/exceptions/DatabaseException.java
+++ src/model/exceptions/DatabaseException.java
@@ -1,12 +1,9 @@
 package model.exceptions;
 
+import java.sql.SQLException;
+
 public class DatabaseException extends RuntimeException {
-    public DatabaseException(String message) {
+    public DatabaseException(String message, SQLException e) {
         super(message);
     }
-    public DatabaseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-    
-    
 }
